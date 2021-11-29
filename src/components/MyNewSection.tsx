969c4798780ec66c34571e3c78cd911ee47edc72
Stackbit Code Editor <projects@stackbit.com>
2021-11-29 15:21:35 +0000

    MyNewSection.tsx: changed

--- src/components/MyNewSection.tsx
+++ src/components/MyNewSection.tsx
@@ -2,7 +2,7 @@ export default function MyNewSection({ content, text }) {
     return (
         <div>
             <h1 data-sb-field-path=".content">{content}</h1>
-            <p>{text}</p>
+            <p data-sb-field-path=".text">{text}</p>
         </div>
     )
 }
